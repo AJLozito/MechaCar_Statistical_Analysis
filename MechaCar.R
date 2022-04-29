8f51526231cdf784246b2dcbaeb8700cc67284f9
AJLozito <96176817+AJLozito@users.noreply.github.com>
2022-04-28 20:24:12 -0400

    Add files via upload

---
+++ MechaCar.R
@@ -0,0 +1,5 @@
+# Linear regression using the lm() function
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
+
+# Summary() function, p-value, and r-squared value for the linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
