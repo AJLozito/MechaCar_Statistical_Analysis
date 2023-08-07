c947735fc5dfcc17b52709102e522c94daef3a5b
AJLozito <96176817+AJLozito@users.noreply.github.com>
2023-08-07 19:22:57 -0400

    Update MechaCar.R

--- MechaCar.R
+++ MechaCar.R
@@ -1,24 +1,11 @@
 library(dplyr)
 
-# Deliverable 1
-
 # Linear regression using the lm() function
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
 
 # Summary() function, p-value, and r-squared value for the linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
 
-
-# Deliverable 2
-
-# Create a total_summary data frame to illustrate mean, median, variance, and standard deviation
-mecha_coil <- read.csv("C:/Users/ajloz/OneDrive/Desktop/UCF Bootcamp/Module 15/Challenge - 15/Suspension_Coil.csv")
-
-total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI),
-                                          Median_PSI=median(PSI),
-                                          Var_PSI=var(PSI),
-                                          Std_Dev_PSI=sd(PSI))
-View(total_summary)
 
 # Create a lot summary
 lot_summary <- mecha_coil %>%
@@ -28,9 +15,6 @@ lot_summary <- mecha_coil %>%
                          Variance=var(PSI),
                          SD=sd(PSI))
 View(lot_summary)
-
-
-# Deliverable 3
 
 # Create T-Tests
 
